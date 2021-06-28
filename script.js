5964087fb38748198957f9292bb53d0cc86db465
Ayman Elgad <aymanelgad65@gmail.com>
2021-06-28 16:57:43 +0100

    the final final

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const togglebtns = document.querySelectorAll('.fa-heart');
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-togglebtns.forEach((btn)=>{
+togglebtns.forEach((btn) => {
     btn.addEventListener('click', () => {
         btn.classList.toggle('toggle-pressed');
     })
-})
+});
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
